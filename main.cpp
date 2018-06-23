d31a967554b266c7c3e890f64e38f4490f46be39
Vadim808 <vch.98@mail.ru>
2018-06-23 17:38:05 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,12 +4,10 @@
 #include<map>
 #include<iomanip>
 
-using namespace std;
-
 struct edge {
     int u, v;
     double time;
-    void put(int a, int b, double c) {
+    edge(int a, int b, double c) {
         u = a;
         v = b;
         time = c;
@@ -19,23 +17,23 @@ struct edge {
 struct next_city {
     int city;
     double time;
-    void put(int c, double t) {
+    next_city(int c, double t) {
         city = c;
         time = t;
     }
 };
 
-vector<int> d;
-vector<int> way;
-int INF = 1000000000;
+std::vector<int> d;
+std::vector<int> way;
+static constexpr int INF = std::numeric_limits<int>::max();
 
-void dijkstra(vector<vector<next_city>>& g, map<pair<int, int>, int>& po,
-              vector<double>& p, int n, int k) {
+void dijkstra(std::vector<std::vector<next_city>>& g, std::map<std::pair<int, int>, int>& po,
+              std::vector<double>& p, int n, int k) {
     d.resize(n);
     for (int i = 1; i < n; ++i) {
         d[i] = INF;
     }
-    vector<bool> was;
+    std::vector<bool> was;
     was.resize(n);
     for (int i = 0; i < n; ++i) {
         int v = -1;
@@ -47,11 +45,11 @@ void dijkstra(vector<vector<next_city>>& g, map<pair<int, int>, int>& po,
         was[v] = true;
         
         for (size_t j = 0; j < g[v].size(); ++j) {
-            if ((v != 0 && po[make_pair(v, g[v][j].city)] <= 0) ||
-                (v == 0 && po[make_pair(v, g[v][j].city)] < k)) {
+            if ((v != 0 && po[{v, g[v][j].city}] <= 0) ||
+                (v == 0 && po[{v, g[v][j].city}] < k)) {
                 int to = g[v][j].city;
                 double len;
-                if (po[make_pair(v, g[v][j].city)] < 0) {
+                if (po[{v, g[v][j].city}] < 0) {
                     len = -g[v][j].time + p[v] - p[to];
                 } else {
                     len = g[v][j].time + p[v] - p[to];
@@ -65,9 +63,9 @@ void dijkstra(vector<vector<next_city>>& g, map<pair<int, int>, int>& po,
     }
 }
 
-vector<int> wass;
+std::vector<int> wass;
 
-void dfs(vector<vector<next_city>>& g, map<pair<int, int>, int>& poi, int v, int K) {
+void dfs(std::vector<std::vector<next_city>>& g, std::map<std::pair<int, int>, int>& poi, int v, int K) {
     wass[v] = 1;
     if (v == K) {
         return;
@@ -76,13 +74,13 @@ void dfs(vector<vector<next_city>>& g, map<pair<int, int>, int>& poi, int v, int
         if (wass[K] == 1) {
             return;
         }
-        if (poi[make_pair(v, g[v][i].city)] == 1) {
+        if (poi[{v, g[v][i].city}] == 1) {
             if (wass[K] == 1) {
                 return;
             }
             way[g[v][i].city] = v;
-            poi[make_pair(v, g[v][i].city)] = 0;
-            poi[make_pair(g[v][i].city, v)] = 0;
+            poi[{v, g[v][i].city}] = 0;
+            poi[{g[v][i].city, v}] = 0;
             dfs(g, poi, g[v][i].city, K);
             if (wass[K] == 1) {
                 return;
@@ -91,79 +89,73 @@ void dfs(vector<vector<next_city>>& g, map<pair<int, int>, int>& poi, int v, int
     }
 }
 
+void add_value3(std::map<std::pair<int, int>, int>& m, int c1, int c2, int c3, int v) {
+    m[{c1, c3}] = v;
+    m[{c3, c2}] = v;
+    m[{c2, c3}] = v;
+    m[{c3, c1}] = v;
+}
+
+void add_value2(std::map<std::pair<int, int>, int>& m, int c1, int c2, int v) {
+    m[{c1, c2}] = v;
+    m[{c2, c1}] = v;
+}
+
+void g_add_value(std::vector<std::vector<next_city>>& g, int c1, int c2, int c3,
+                 next_city& nc1, next_city& nc2, next_city& nc3) {
+    g[c3].push_back(nc1);
+    g[c3].push_back(nc2);
+    g[c1].push_back(nc3);
+    g[c2].push_back(nc3);
+}
+
 int main() {
-    cout.precision(15);
+    std::cout.precision(15);
     int N, M, k;
-    cin >> N >> M >> k;
+    std::cin >> N >> M >> k;
     int m = M;
     int n = N;
-    vector<vector<next_city>> g;
-    vector<double> p;
-    vector<edge> edges;
-    map<pair<int, int>, int> points;
+    std::vector<std::vector<next_city>> g;
+    std::vector<double> p;
+    std::vector<edge> edges;
+    std::map<std::pair<int, int>, int> points, ed, ed1;
     g.resize(n + 1);
-    int city1, city2;
+    int city1, city2, city3;
     double time;
-    next_city nc;
-    nc.put(1, 0);
-    points[make_pair(1, 0)] = 0;
-    points[make_pair(0, 1)] = 0;
-    edge e;
-    e.put(0, 1, 0);
+    next_city nc(1, 0);
+    points[{1, 0}] = 0;
+    points[{0, 1}] = 0;
+    edge e(0, 1, 0);
     edges.push_back(e);
     g[0].push_back(nc);
-    vector<vector<int>> nn;
+    std::vector<std::vector<int>> nn;
     nn.resize(n + 1);
     for (int i = 0; i < n + 1; ++i) {
         nn[i].resize(n + 1);
     }
-    map<pair<int, int>, int> ed;
-    map<pair<int, int>, int> ed1;
-    int city3;
     for (int i = 0; i < M; ++i) {
-        cin >> city1 >> city2 >> time;
+        std::cin >> city1 >> city2 >> time;
         if (nn[city1][city2] == 1) {
             n += 1;
             city3 = n;
-            points[make_pair(city1, city3)] = 0;
-            points[make_pair(city3, city2)] = 0;
-            points[make_pair(city2, city3)] = 0;
-            points[make_pair(city3, city1)] = 0;
-            ed[make_pair(city1, city3)] = i;
-            ed[make_pair(city3, city2)] = i;
-            ed[make_pair(city3, city1)] = i;
-            ed[make_pair(city2, city3)] = i;
+            add_value3(points, city1, city2, city3, 0);
+            add_value3(ed, city1, city2, city3, i);
             m += 1;
-            ed1[make_pair(city1, city3)] = edges.size();
-            ed1[make_pair(city3, city2)] = edges.size() ;
-            ed1[make_pair(city3, city1)] = edges.size();
-            ed1[make_pair(city2, city3)] = edges.size();
+            add_value3(ed1, city1, city2, city3, static_cast<int>(edges.size()));
             g.resize(n + 1);
-            next_city nc1, nc2, nc3;
-            nc1.put(city2, time / 2); nc2.put(city1, time / 2);
-            nc3.put(city3, time / 2);
-            g[city3].push_back(nc1);
-            g[city3].push_back(nc2);
-            g[city1].push_back(nc3);
-            g[city2].push_back(nc3);
-            edge e1, e2;
-            e1.put(city1, city3, time / 2);
-            e2.put(city3, city2, time / 2);
+            next_city nc1(city2, time / 2), nc2(city1, time / 2), nc3(city3, time / 2);
+            g_add_value(g, city1, city2, city3, nc1, nc2, nc3);
+            edge e1(city1, city3, time / 2), e2(city3, city2, time / 2);
             edges.push_back(e1);
             edges.push_back(e2);
         } else {
             nn[city1][city2] = 1;
             nn[city2][city1] = 1;
-            points[make_pair(city1, city2)] = 0;
-            points[make_pair(city2, city1)] = 0;
-            ed[make_pair(city1, city2)] = i;
-            ed[make_pair(city2, city1)] = i;
-            ed1[make_pair(city1, city2)] = edges.size();
-            ed1[make_pair(city2, city1)] = edges.size();
-            next_city nc1, nc2;
-            nc1.put(city2, time); nc2.put(city1, time);
-            edge e;
-            e.put(city1, city2, time);
+            add_value2(points, city1, city2, 0);
+            add_value2(ed, city1, city2, i);
+            add_value2(ed1, city1, city2, static_cast<int>(edges.size()));
+            next_city nc1(city2, time), nc2(city1, time);
+            edge e(city1, city2, time);
             edges.push_back(e);
             g[city1].push_back(nc1);
             g[city2].push_back(nc2);
@@ -193,7 +185,7 @@ int main() {
     while (way[N] != -1) {
         place = N;
         while (true) {
-            pair<int, int> my_pair1(way[place], place), my_pair2(place, way[place]);
+            std::pair<int, int> my_pair1(way[place], place), my_pair2(place, way[place]);
             points[my_pair1] += 1;
             points[my_pair2] -= 1;
             if (points[my_pair1] == 1 && points[my_pair2] == 1) {
@@ -215,11 +207,11 @@ int main() {
         way[N] = -1;
         dijkstra(g, points, p, n + 1, k);
     }
-    if (points[make_pair(0, 1)] != k) {
-        cout << -1;
+    if (points[{0, 1}] != k) {
+        std::cout << -1;
     } else {
         double t = 0;
-        vector<vector<int>> ways;
+        std::vector<std::vector<int>> ways;
         ways.resize(k);
         
         for (int i = 0; i < k; ++i) {
@@ -231,12 +223,12 @@ int main() {
             dfs(g, points, 1, N);
             int place = N;
             while (way[place] != 0) {
-                ways[i].push_back(ed[make_pair(place, way[place])]);
-                t = t + edges[ed1[make_pair(place, way[place])]].time;
+                ways[i].push_back(ed[{place, way[place]}]);
+                t = t + edges[ed1[{place, way[place]}]].time;
                 place = way[place];
             }
         }
-        cout << t / k  << "\n";
+        std::cout << t / k  << "\n";
         for (int i = 0; i < k; ++i) {
             size_t len = ways[i].size();
             for (size_t j = 0; j < ways[i].size(); ++j) {
@@ -245,13 +237,13 @@ int main() {
                     ways[i][j] = -1;
                 }
             }
-            cout << len << ' ';
+            std::cout << len << ' ';
             for (size_t j = 0; j < ways[i].size(); ++j) {
                 if (ways[i][ways[i].size() - j - 1] != -1) {
-                    cout << ways[i][ways[i].size() - j - 1] + 1 << ' ';
+                    std::cout << ways[i][ways[i].size() - j - 1] + 1 << ' ';
                 }
             }
-            cout << '\n';
+            std::cout << '\n';
         }
     }
 }
